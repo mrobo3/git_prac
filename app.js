d2922f37d88a2c8ad0330b4ceda1e7bde498fc7e
oh <outputor777@gmail.com>
2022-07-08 23:13:10 +0900

    for

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
